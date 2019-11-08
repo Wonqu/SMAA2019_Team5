a97f041ad4cee59fa6841c5902dde0894d625cb4
Jarema27 <jaremaradom@gmail.com>
2019-11-08 18:53:59 +0100

    Fasttext adjusted dataset and script to generate it

--- adjust_to_fasttext.py
+++ adjust_to_fasttext.py
@@ -7,15 +7,16 @@ os.chdir("./")
 symbols = []
 out = []
 extension = 'csv'
-spooks = pd.read_csv(glob.glob('data/spooky-author-identification/train/train.csv')[0])
+spooks = pd.read_csv(glob.glob('data/spooky-author-identification/train.csv')[0])
 
 
 merged_csv = []
 
 for row in spooks .iterrows():
-    merged_csv.append(['__label__' + row[1]['author'], row[1]['text']])
+    print(row[1])
+    merged_csv.append(['__label__' + row[1]['author'] + " " + row[1]['text']])
 
 
-df = pd.DataFrame(merged_csv, columns=['label', 'text'])
+df = pd.DataFrame(merged_csv, columns=['label'])
 print(df)
-pd.DataFrame(df).to_csv("data/out/spook_to_fasttext.csv", index=False, sep=' ', header=False)
+pd.DataFrame(df).to_csv("data/out/spook_to_fasttext.csv", index=False, header=False)
