10fb0b0e8a144681f6529e04c507705d84794b43
Jarema27 <jaremaradom@gmail.com>
2019-11-23 22:44:54 +0100

    New data included in adjust scripts

--- adjust_to_fasttext.py
+++ adjust_to_fasttext.py
@@ -8,13 +8,20 @@ symbols = []
 out = []
 extension = 'csv'
 spooks = pd.read_csv(glob.glob('data/spooky-author-identification/train.csv')[0])
+eap = pd.read_csv(glob.glob('data/out/eap.csv')[0])
+hpl = pd.read_csv(glob.glob('data/out/hpl.csv')[0])
+mws = pd.read_csv(glob.glob('data/out/mws.csv')[0])
 
+mamatrix = [spooks. as_matrix(), eap.as_matrix(), hpl.as_matrix(), mws.as_matrix()]
 
 merged_csv = []
 
-for row in spooks .iterrows():
-    print(row[1])
-    merged_csv.append(['__label__' + row[1]['author'] + " " + row[1]['text']])
+# we iterate to length of longest
+for i in range(0, len(spooks)):
+    for matrix in mamatrix:
+        if i < len(matrix):
+            print(matrix[i])
+            merged_csv.append(['__label__' + matrix[i][2] + ' ' + matrix[i][1]])
 
 
 df = pd.DataFrame(merged_csv, columns=['label'])
